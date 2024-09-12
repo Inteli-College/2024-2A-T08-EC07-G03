38da1227ae9138f75be823ac1076bcd6015d2dc5
LuizFernandoLeao <luiz.leao@sou.inteli.edu.br>
2024-09-12 10:38:30 -0300

    fix: header tela de treinamento

--- frontend/app-typescript/src/components/Header.tsx
+++ frontend/app-typescript/src/components/Header.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-// Mapeamento das rotas para os nomes das páginas
 const pageNames: { [key: string]: string } = {
   '/': 'Home',
-  '/training': 'Treinar',
+  '/training': 'Treinar',  
+  '/progress': 'Treinar',
   '/trained': 'Modelo treinado',
-  '/trained-model/previous': 'Modelo Anterior',
-  '/trained-model/current': 'Modelo Atual',
-  // Adicione aqui outras rotas conforme necessário
 };
 
 const Header: React.FC = () => {
@@ -19,7 +16,7 @@ const Header: React.FC = () => {
     <header className="flex items-center space-x-4 p-4">
       <Link to="/" className="text-gray-400 hover:text-white">Home</Link>
       <span className="text-gray-400">{'>'}</span>
-      
+
       <span className="text-white">{currentPage}</span>
     </header>
   );
