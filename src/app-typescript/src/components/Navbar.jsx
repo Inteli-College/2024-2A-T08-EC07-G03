1e5ff6f7372c007ce6ec5105f4e1760e6724b9f9
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-09-13 01:08:06 -0300

    fix: tirando arquivos dulicados

--- src/app-typescript/src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const Navbar = ({ children }) => {
-    return (<nav className="bg-transparent text-white text-lg m-6">
-      {children}
-    </nav>);
-};
-export default Navbar;
