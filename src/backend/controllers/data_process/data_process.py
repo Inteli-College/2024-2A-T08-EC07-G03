375f03c748c62258e6a392001dc246fa39ab5f83
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-09-13 15:16:01 -0300

    faet: salvando input do modelo no banco de dados

--- src/backend/controllers/data_process/data_process.py
+++ src/backend/controllers/data_process/data_process.py
@@ -2,6 +2,18 @@ import pandas as pd
 import numpy as np
 import io
 from  controllers.datalake.file_manager import download_file
+from routers.crud import insert_data_entries
+from datetime import datetime
+from pydantic import BaseModel
+
+class NewEntries(BaseModel):
+    date: datetime
+    nvezes1: int
+    nvezes2: int
+    nvezes718: int
+    somatempo1: int
+    somatempo2: int
+    somatempo718: int
 
 async def build_feature_prediction(knr):
     # Pegar os dados do arquivo
@@ -24,6 +36,22 @@ async def build_feature_prediction(knr):
 
     print(file_data.values)
 
+    # Montar dicionario para enviar para o banco de dados
+    db_data = {
+        "date": datetime.now(),
+        "nvezes1": file_data["Nvezes1"].values[0],
+        "nvezes2": file_data["Nvezes2"].values[0],
+        "nvezes718": file_data["Nvezes718"].values[0],
+        "somatempo1": file_data["SomaTempo1"].values[0],
+        "somatempo2": file_data["SomaTempo2"].values[0],
+        "somatempo718": file_data["SomaTempo718"].values[0],
+    }
+    
+    db_data_pydantic = NewEntries(**db_data)
+    
+    insert_data_entries(db_data_pydantic)
+
+    
     file_data = file_data[
         [
             "Nvezes1",
@@ -34,10 +62,10 @@ async def build_feature_prediction(knr):
             "SomaTempo718",
         ]
     ].values
-    
+
     # Add um valor aleatório que indica o TemFalhaRod
     file_data = np.hstack((file_data, np.random.rand(file_data.shape[0], 1))) # TIRAR ISSO DEPOIS !!!!GAMBIARA!!!!
-
+        
     # Reshape para o formato que o Keras espera
     file_data = file_data.reshape(1, 1, -1)  # (1, 1, 7)
     
