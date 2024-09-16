ee00e12a38e8878b45701984e5894e81de778fd5
Rodrigo-07 <freirerodrigosales@gmail.com>
2024-09-16 11:33:01 -0300

    fix: rota de health check e dockers

--- src/backend/routers/__init__.py
+++ src/backend/routers/__init__.py
@@ -3,6 +3,7 @@ from .hello import router as hello
 from .model import router as model
 from .crud import router as crud
 from .datalake import router as datalake
+from .health import router as healthCheck
 
 router = APIRouter()
 
@@ -10,3 +11,4 @@ router.include_router(hello, prefix="/hello")
 router.include_router(model, prefix="/model")
 router.include_router(crud, prefix="/crud")
 router.include_router(datalake, prefix="/datalake")
+router.include_router(healthCheck, prefix="/healthcheck")
