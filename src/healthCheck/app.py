e1116b5d6502efaf7dd5ce7463215eef11b13163
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-09-18 00:54:02 -0300

    teste inicial de servico de heaalth check

---
+++ src/healthCheck/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/index")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
