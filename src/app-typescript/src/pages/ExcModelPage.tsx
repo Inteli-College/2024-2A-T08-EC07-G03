25a4214e3206503f91170b152fb681fd4c3cbe47
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-10-10 14:16:19 -0300

    fix: arrumando o arquivo tsx

--- src/app-typescript/src/pages/ExcModelPage.tsx
+++ src/app-typescript/src/pages/ExcModelPage.tsx
@@ -1,55 +1,56 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
-import ConfirmationPopup from '../components/ConfirmationPopup';
 import { useLocation } from 'react-router-dom';
 
+// Definir os tipos de estado que podem vir do 'location.state'
+interface LocationState {
+    result_atual?: string;
+    knr?: string;
+}
 
 const ExcModelPage: React.FC = () => {
-  const [isPopupVisible, setPopupVisible] = useState(false);
-
-  const location = useLocation();
-
-  const { result_atual } = location.state || {};
-  
-  console.log(result_atual);
-
-  const handleOpenPopup = () => {
-    setPopupVisible(true);
-  };
-
-  const handleClosePopup = () => {
-    setPopupVisible(false);
-  };
-
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-[#333641] to-[#282A32] text-white p-8">
-      <Header />
-      <div className="flex flex-col items-center justify-center" >
-      <h1 className="text-3xl font-light mb-6">Modelo Executado:</h1>
-
-      <div className="w-full max-w-2xl h-64 bg-gray-200 rounded-lg flex items-center justify-center text-black text-2xl mb-8">
-        Gráficos e Métricas
-      </div>
-
-      <div className="w-full max-w-2xl mb-4">
-        <label className="block text-gray-400 mb-2">Resultado Atual:</label>
-        <div className="w-full bg-gray-300 h-10 rounded-lg flex justify-center items-center">
-          <span className="text-black">{result_atual}</span>
-        </div>
-      </div>
-
-      <div className="w-full max-w-2xl mb-4">
-        <label className="block text-gray-400 mb-2">Modelo Anterior:</label>
-        <div className="w-full bg-gray-300 h-10 rounded-lg flex justify-center items-center">
-          <span className="text-black">XXXXXX</span>
-        </div>
-      </div>
-
+    const [isPopupVisible, setPopupVisible] = useState(false);
     
-     
-    </div>
-    </div>
-  );
+    // Adiciona tipagem ao useLocation para garantir que os valores de estado estão corretos
+    const location = useLocation<{ state: LocationState }>();
+    
+    // Extrai os valores de estado de forma segura
+    const { result_atual, knr } = location.state || {};
+    console.log(result_atual);
+
+    const handleOpenPopup = () => {
+        setPopupVisible(true);
+    };
+
+    const handleClosePopup = () => {
+        setPopupVisible(false);
+    };
+
+    return (
+        <div className="min-h-screen bg-gradient-to-b from-[#333641] to-[#282A32] text-white p-8">
+            <Header />
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-4xl font-light mb-24 mt-8">Modelo Executado:</h1>
+
+                <div className="w-full shadow-2xl max-w-2xl h-32 bg-[#2F323C] justify-center">
+                    <div className="text-white text-lg mb-2 mt-4 text-center">
+                        Resultado da Predição:
+                    </div>
+                    <div className="text-white text-1xl text-center opacity-75">
+                        KNR executado: {knr}
+                    </div>
+                </div>
+                <div className="w-full max-w-2xl mb-4 mt-24">
+                    <div className={`w-full shadow-2xl max-w-2xl h-32 flex flex-col items-center justify-center ${ result_atual === 'Tem falha' ? 'bg-red-500' : 'bg-green-500'}`}>
+                    <label className="block font-black text-white opacity-75 mt-4">Resultado:</label>
+                    <span className="text-white opacity-75 mt-2">
+                        {result_atual === 'Tem falha' ? 'Tem falha' : 'Não tem falha'}
+                    </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ExcModelPage;
